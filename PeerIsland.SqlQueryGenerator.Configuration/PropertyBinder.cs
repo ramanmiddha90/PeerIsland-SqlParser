1d2e10dfa9c9907260a14ba0c30bcf06bb27dd2a
Raman Kumar <raman.kumar@assurant.com>
2021-08-08 17:49:01 +0530

    changes

---
+++ PeerIsland.SqlQueryGenerator.Configuration/PropertyBinder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+namespace PeerIsland.SqlQueryGenerator.Configuration
+{
+   public static class PropertyBinder
+    {
+        public static T BindProperty<T>(string PropertyKey, IDictionary<string, IDictionary<string, object>> ClauseSection)
+        {
+            if (ClauseSection.ContainsKey(PropertyKey))
+                return (T)ClauseSection.Where(t => t.Key == PropertyKey).FirstOrDefault().Value;
+
+            return default(T);
+        }
+    }
+}
